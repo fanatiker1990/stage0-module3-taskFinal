d6af8e8c1832c4a378ba5bb5d70d3009437c7ac2
fanatiker1990 <59375292+fanatiker1990@users.noreply.github.com>
2023-05-04 16:40:54 +0300

    1

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,13 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        int remainder=number%10;
+        int remainderDividedBy_1000=number/1000;
+        number%=1000;
+        int remainderDividedBy_100=number/100;
+        number%=100;
+        int remainderDividedBy_10=number/10;
+        System.out.println(remainderDividedBy_1000+remainderDividedBy_100+remainderDividedBy_10+remainder);
     }
+
 }
--- src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
@@ -1,6 +1,9 @@
 package lang.print.gaps.finalModuleTask;
 
 public class TemperatureConverter {
-    public void toFahrenheit(int temperatureCelsius){
+    public void toFahrenheit(int temperatureCelsius) {
+        double temperatureFahrenheit = ((1.8) * temperatureCelsius) + 32;
+        System.out.println(temperatureFahrenheit);
     }
+
 }
